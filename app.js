2fddc3aa5736ad490f39d5620782b179a0afd5cc
onlinefred <67547195+laterinthepast@users.noreply.github.com>
2021-12-03 14:30:40 +0000

    added phoros

--- app.js
+++ app.js
@@ -27,19 +27,41 @@ mobileToggle.addEventListener('click', function () {
 
 let modalk = document.querySelector('.galleryBkg');
 let closeModal = document.querySelector('.toggle');
-let gallery = document.querySelector('.gallery-wrapper');
+let gallery = document.querySelector('.gallery');
 
 
 gallery.addEventListener('click', function (e) {
     if (e.target.classList.contains('myImg')) {
         e.target.classList.add('active');
         modalk.style.display = 'block';
+        console.log("clicked");
         closeModal.addEventListener('click', function () {
             modalk.style.display = 'none';
             e.target.classList.remove('active');
-        })
+        });
     }
 
 })
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
 
 
