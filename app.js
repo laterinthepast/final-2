cdcbe48100a3f3cec9338f900092cc601e257340
onlinefred <67547195+laterinthepast@users.noreply.github.com>
2021-12-03 13:04:31 +0000

    added gallery

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ let mobileToggle = document.querySelector('.mobile-nav');
 let mobileLinks = document.querySelector('.navbar-links');
 let modal = document.querySelector('.modal');
 let navbar = document.querySelector('.navbar');
-/* let image = document.querySelector('.gallery-wrapper');
-let modalImage = document.querySelector('.myImg');
-let modalG = document.querySelector('.galleryBkg');
-let toggle = document.querySelector('.toggle'); */
+
 
 
 mobileToggle.addEventListener('click', function () {
@@ -26,11 +23,23 @@ mobileToggle.addEventListener('click', function () {
 
 });
 
-$('#myCarousel').on('slid.bs.carousel', function (e) {
-    $('#myCarousel').carousel('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
-  })
-  
-  $('#myCarousel').carousel('1') // Will start sliding to the slide 1 and returns to the caller
-  $('#myCarousel').carousel('2') // !! Will be ignored, as the transition to the slide 1 is not finished !!
+/* MODAL IMAGE GALLERY */
+
+let modalk = document.querySelector('.galleryBkg');
+let closeModal = document.querySelector('.toggle');
+let gallery = document.querySelector('.gallery-wrapper');
+
+
+gallery.addEventListener('click', function (e) {
+    if (e.target.classList.contains('myImg')) {
+        e.target.classList.add('active');
+        modalk.style.display = 'block';
+        closeModal.addEventListener('click', function () {
+            modalk.style.display = 'none';
+            e.target.classList.remove('active');
+        })
+    }
+
+})
 
 
